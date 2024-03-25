aa78383ebb6015e3edfe43110a2a44df83ff248a
Yashrif Arifin <yaswoccho@gmail.com>
2024-03-25 20:58:03 +0600

    feat: add logo & styling

---
+++ src/app/(root)/icon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og';
+
+import Logo from '@components/Logo';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = 'image/png';
+
+const Icon = () => {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: 'none',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 5,
+        }}
+      >
+        <Logo
+          style={size}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+};
+
+export default Icon;

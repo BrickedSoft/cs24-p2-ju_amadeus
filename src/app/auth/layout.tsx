f19c0ffe4b9335a073e4ef138b758b11fd8b2faf
Yashrif Arifin <yaswoccho@gmail.com>
2024-03-27 22:17:04 +0600

    fix: logo link

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/Logo";
 import AuthImage from "./AuthImage";
+import Link from "next/link";
 
 type AuthProps = {
   children: React.ReactNode;
@@ -9,7 +10,12 @@ const AuthLayout: React.FC<AuthProps> = ({ children }) => {
   return (
     <section className="w-full h-screen flex justify-center items-center">
       <main className="relative grid md:grid-cols-[minmax(200px,auto),1fr] gap-8 md:gap-16 items-center justify-between p-8 md:p-12 bg-white rounded-lg">
-        <Logo className="md:absolute mx-auto -mb-4 md:top-12 md:left-12 w-12 h-12" />
+        <Link
+          href="/"
+          className="md:absolute mx-auto -mb-4 md:top-12 md:left-12"
+        >
+          <Logo className="w-12 h-12" />
+        </Link>
         <AuthImage height={"full"} width={"auto"} className="hidden md:block" />
         {children}
       </main>

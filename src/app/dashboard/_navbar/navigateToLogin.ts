09ce2588967d2c9c27d6981f0bd859255a718da2
Jahid Hossain <jahid.hossain.e99@gmail.com>
2024-03-28 18:45:41 +0600

    logout clears session

--- src/app/dashboard/_navbar/navigateToLogin.ts
+++ src/app/dashboard/_navbar/navigateToLogin.ts
@@ -5,9 +5,14 @@ import { cookies } from 'next/headers'
 
 export async function navigateToLogin(data: FormData) {
   const oneDay = 24 * 60 * 60 * 1000
+  const userId = cookies().get("userId")?.value
+  const token = cookies().get("token")?.value
+
+  await prisma?.loginToken.deleteMany({ where: { id: userId, token: token } })
+
   cookies().set('token', 'value', { expires: Date.now() - oneDay })
   cookies().set('userId', 'value', { expires: Date.now() - oneDay })
   cookies().set('role', 'value', { expires: Date.now() - oneDay })
 
-  redirect('/auth/login')
+  redirect('/')
 }

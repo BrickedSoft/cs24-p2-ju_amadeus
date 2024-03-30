90110d2f46baf4b44b9808b6768275935d4e9851
Yashrif Arifin <yaswoccho@gmail.com>
2024-03-30 16:33:19 +0600

    feat: add route checking (#41)

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,12 +1,20 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
 import Logo from "@/components/Logo";
 import AuthImage from "./AuthImage";
-import Link from "next/link";
 
 type AuthProps = {
   children: React.ReactNode;
 };
 
 const AuthLayout: React.FC<AuthProps> = ({ children }) => {
+  const cookieStore = cookies();
+  if (cookieStore.has("token")) {
+    notFound();
+  }
+
   return (
     <section className="w-full h-screen flex justify-center items-center">
       <main className="relative grid md:grid-cols-[auto,1fr] gap-8 md:gap-16 items-center justify-between p-8 md:p-12 bg-white rounded-lg">
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,18 @@
+import { notFound } from "next/navigation";
+import { cookies } from "next/headers";
+
 import Navbar from "./_navbar/Navbar";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  if (!cookieStore.has("token")) {
+    notFound();
+  }
+
   return (
     <div>
       <Navbar />

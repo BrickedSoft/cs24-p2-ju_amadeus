fdc402752330a84da9b9bbb4eb26c6cf2ce96873
Yashrif Arifin <yaswoccho@gmail.com>
2024-03-30 16:31:54 +0600

    feat: add route checking

--- src/app/dashboard/_navbar/navigateToLogin.ts
+++ src/app/dashboard/_navbar/navigateToLogin.ts
@@ -1,18 +1,18 @@
 "use server";
 
-import { redirect, RedirectType } from "next/navigation";
 import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+
+import { routes } from "@/assets/data/routes";
 
 export async function navigateToLogin(data: FormData) {
-  const oneDay = 24 * 60 * 60 * 1000;
   const userId = cookies().get("userId")?.value;
   const token = cookies().get("token")?.value;
 
   await prisma?.loginToken.deleteMany({ where: { id: userId, token: token } });
 
-  cookies().set("token", "value", { expires: Date.now() - oneDay });
-  cookies().set("userId", "value", { expires: Date.now() - oneDay });
-  cookies().set("role", "value", { expires: Date.now() - oneDay });
+  cookies().delete("userId");
+  cookies().delete("token");
 
-  redirect("/", RedirectType.replace);
+  redirect(routes.home, RedirectType.replace);
 }

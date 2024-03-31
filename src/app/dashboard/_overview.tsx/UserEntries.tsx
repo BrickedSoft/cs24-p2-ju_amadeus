06c98d476d59f80124d78f5705161121a550123d
Yashrif Arifin <yaswoccho@gmail.com>
2024-03-31 19:56:50 +0600

    feat: overview page

---
+++ src/app/dashboard/_overview.tsx/UserEntries.tsx
@@ -0,0 +1,56 @@
+import { cookies } from "next/headers";
+
+import { userDataEndpoint } from "@/assets/data/api/endpoints";
+import { People } from "@/components/Icons";
+
+const UserEntries = async () => {
+  const cookieStore = cookies();
+  const userList = await fetch(`${userDataEndpoint}`, {
+    cache: "no-store",
+    // @ts-ignore
+    headers: {
+      cookie: cookieStore,
+    },
+  }).then(async (res) => {
+    const data = await res.json();
+    return data;
+  });
+
+  let userCount = {
+    STSManager: 0,
+    SystemAdmin: 0,
+    LandfillManager: 0,
+  };
+
+  userList.users.filter((user: any) => {
+    userCount[user.role as keyof typeof userCount]++;
+  });
+
+  const users = {
+    STSManager: "STS Manager",
+    SystemAdmin: "System Admin",
+    LandfillManager: "Landfill Manager",
+  };
+
+  const colors = ["#048A52", "#339AF0", "#FF922B"];
+
+  return (
+    <div className="flex gap-4 justify-between items-center px-12 py-8 border border-gray-300 rounded-lg">
+      {Object.values(userCount).map((item, index) => (
+        <div key={index} className="flex flex-col gap-4">
+          <h2 className={`heading-tertiary text-primary text-[${colors[index]}]`}>
+            {users[Object.keys(userCount)[index] as keyof typeof users]}
+          </h2>
+          <div className="flex gap-4 items-center">
+            <People className={`fill-primary fill-[${colors[index]}] w-8 h-8`} />
+            <p className={`text-3xl text-primary text-[${colors[index]}] font-semibold`}>
+              {item}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UserEntries;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,48 @@
-import TokenAlert from "./_components/TokenAlert";
+import { cookies } from "next/headers";
 
-const Dashboard: React.FC = () => {
+import { userDataEndpoint } from "@/assets/data/api/endpoints";
+import TokenAlert from "./_components/TokenAlert";
+import TotalEntries from "./_overview.tsx/TotalEntries";
+import UserEntries from "./_overview.tsx/UserEntries";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
+
+const Dashboard: React.FC = async () => {
+  const cookieStore = cookies();
+  const userData = await fetch(
+    `${userDataEndpoint}/${cookieStore.get("userId")?.value}`,
+    {
+      cache: "no-store",
+      // @ts-ignore
+      headers: {
+        cookie: cookieStore,
+      },
+    }
+  ).then(async (res) => {
+    const data = await res.json();
+    return data;
+  });
+
   return (
     <main className="w-full">
       <TokenAlert />
-      <p className="">Welcome to Dashboard!</p>
+      <div className="max-w-6xl px-5 mx-auto my-12 flex flex-col gap-8">
+        <Suspense fallback={<Loading />}>
+          <h1 className="heading-secondary text-primary">
+            Welcome {userData.user.name}
+          </h1>
+        </Suspense>
+        {userData.user.role === "SystemAdmin" && (
+          <>
+            <Suspense fallback={<Loading />}>
+              <TotalEntries />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
+              <UserEntries />
+            </Suspense>
+          </>
+        )}
+      </div>
     </main>
   );
 };

eaf2bd31eb3ab188f7d2af5553607ed6e6737b70
Jahid Hossain <jahid.hossain.e99@gmail.com>
2024-05-11 00:00:58 +0600

    upcoming events

--- web_app/prisma/migrations/20240510145919_report_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "IssueReport" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "reportType" TEXT,
-    "location" TEXT,
-    "description" TEXT,
-    "userId" TEXT,
-    CONSTRAINT "IssueReport_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
